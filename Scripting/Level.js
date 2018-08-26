9b22ffc0786730bb86e0b8d968e18b9857f98331
unknown <jdboucher@wpi.edu>
2018-08-25 20:44:11 -0400

    Started random level generation :D

---
+++ Scripting/Level.js
@@ -0,0 +1,16 @@
+// level width and height are between 10 and 20 for now
+var levelWidth = 10 + Math.floor(10*Math.random());
+var levelHieght = 10 + Math.floor(10*Math.random());
+
+// create a 2d array of size width x height
+var mapArray = new Array(levelWidth);
+for(var i=0; i<levelWidth; i++){
+    mapArray[i] = new Array(levelHieght);
+}
+
+// populate the array with random values (whole numbers between 0 and 100)
+for(var i=0; i<levelWidth; i++){
+    for(var j=0; j<levelHieght; j++){
+        
+    }
+}
